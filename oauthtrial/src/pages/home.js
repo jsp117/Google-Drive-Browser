017544abc535fff2c4f67df0b9d9a908caca808e
JonMac <jonsp14@gmail.com>
2021-03-11 02:13:48 -0800

    add base components, install tailwind css

---
+++ oauthtrial/src/pages/home.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+export default function main(){
+    return (
+        <Router>
+            Hello
+        </Router>
+    )
+}
